cc99b4fa54620b9ab1a1a28ac9617687ed5f4e2a
tjbo <tom@prototypable.ca>
2025-11-18 11:22:44 -0500

    updates to site

--- src/prismic.js
+++ src/prismic.js
@@ -1,21 +1,18 @@
 import * as prismic from "@prismicio/client";
 
 const isProd = !!import.meta.env && import.meta.env.MODE === "production";
-const rootUrl = isProd
-  ? "https://www.moolahlist.com"
-  : "http://localhost:4321";
+const rootUrl = isProd ? "https://www.moolahlist.com" : "http://localhost:4321";
 
 const client = prismic.createClient("moolahlist");
 const _pages = await client.getAllByType("page");
 const _profiles = await client.getAllByType("profile");
 
-export const profiles = _profiles 
-  .map((p) => {
-    return {
-      ...p,
-      url: rootUrl + "/profile/" + p.uid + "/",
-    };
-  })
+export const profiles = _profiles.map((p) => {
+  return {
+    ...p,
+    url: rootUrl + "/profile/" + p.uid + "/",
+  };
+});
 
 export const pages = [..._pages].map((page) => {
   return {
@@ -24,21 +21,21 @@ export const pages = [..._pages].map((page) => {
   };
 });
 
-export const urls = [...pages, ...profiles].map(
-  (item) => {
-    return {
-      uid: item.uid,
-      id: item.id,
-      last_mod: item.last_publication_date,
-      url: item.url,
-    };
-  },
-);
+export const urls = [...pages, ...profiles].map((item) => {
+  return {
+    uid: item.uid,
+    id: item.id,
+    last_mod: item.last_publication_date,
+    url: item.url,
+  };
+});
 
 export function getUrl(id) {
-  console.log(urls.filter((url) => {
-    return url.id === id;
-  })[0].url)
+  console.log(
+    urls.filter((url) => {
+      return url.id === id;
+    })[0].url,
+  );
   return urls.filter((url) => {
     return url.id === id;
   })[0].url;
@@ -49,3 +46,6 @@ export async function getPage(uid) {
     return page.uid === uid;
   })[0];
 }
+
+const _settings = await client.getAllByType("settings");
+export const settings = _settings[0];
