7600bb6e9b20b2ed07d91eeadc8e84f125c9adca
tjbo <tom@prototypable.ca>
2026-01-09 07:40:21 -0500

    updates

---
+++ src/constants/enums.js
@@ -0,0 +1,33 @@
+export const LOAN_TYPES = {
+  BRIDGE: "Bridge",
+  CASH_OUT: "Cash-out",
+  REFINANCE: "Refinance",
+  FIX_AND_FLIP: "Fix & Flip",
+  GROUND_UP_CONSTRUCTION: "Ground-up Construction",
+  SHORT_TERM_RENTAL: "Short-Term Rental",
+  DSCR: "DSCR Rental",
+  MULTIFAMILY: "Multifamily",
+  MIXED_USE: "Mixed-Use",
+  PORTFOLIO: "Portfolio",
+  COMMERCIAL: "Commercial",
+  LAND: "Land",
+  RENOVATION: "Renovation / Rehab",
+  SPECIAL_PURPOSE: "Special Purpose",
+};
+
+export const PROPERTY_TYPES = {
+  RESIDENTIAL: "Residential",
+  SMALL_MULTIFAMILY: "Small Multifamily",
+  LARGE_MULTIFAMILY: "Large Multifamily",
+  CONDOS: "Condos",
+  TOWNHOMES: "Townhomes",
+  COMMERCIAL: "Commercial",
+  INDUSTRIAL: "Industrial",
+  LAND_OR_DEVELOPMENT: "Land Or Development",
+  SPECIAL_PURPOSE: "Special Purpose",
+  MIXED_USE: "Mixed Use",
+  OFFICE: "Office",
+  RETAIL: "Retail",
+  HOSPITALITY: "Hospitality",
+  SELF_STORAGE: "Self Storage",
+};
